023c9dc47c5b66b83626dcefdacaa11b9b0712c8
Lumieres <asesdsx@gmail.com>
2020-07-13 10:02:20 +0900

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,18 +1,23 @@
 import keras as K
-from keras.layers import Embedding, LSTM, Dense
 from keras.models import Sequential
+from keras.layers import Embedding, Dropout, Convolution1D, MaxPooling1D, LSTM, Dense
+from keras.layers import Bidirectional
 from keras.optimizers import RMSprop
 from keras.losses import binary_crossentropy
 
-def Model(wordSize):
+def Model(setSize, inputLength):
     model = Sequential()
 
-    model.add(Embedding(wordSize, 128))
+    model.add(Embedding(setSize, 256, input_length = inputLength))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(256, 3, strides = 1, padding = 'valid', activation = 'relu'))
+    model.add(MaxPooling1D(pool_size = 2))
+    model.add(LSTM(64, return_sequences = True))
     model.add(LSTM(128))
     model.add(Dense(1, activation = 'sigmoid'))
     
-    model.compile(optimizer = RMSprop(),
-                  loss = binary_crossentropy(),
+    model.compile(optimizer = RMSprop(learning_rate = 1e-5),
+                  loss = 'binary_crossentropy',
                   metrics = ['accuracy'])
 
     return model
